290660da9117f0bb2fe93737d62b3b91821b3875
Christian <christian.watson786@gmail.com>
2020-11-08 14:05:48 +0800

    I got distracted, and tried some things out, one of my most fun days coding! Felt like I was a beginner seeing something flashy again :D

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-function changeImage (imgid, folder, newimg) {
-    document.getElementById(imgid).src = "img/" + folder + "/" + newimg + ".jpg";
+var sideColorPrice = 10;
+var topColorPrice = 10;
+var ribColorPrice = 10;
+var total = sideColorPrice + topColorPrice + ribColorPrice;
+
+function changeImage(id) {
+    document.getElementById(id).onclick = function () {
+        var select = document.getElementById('select');
+        var options = this.querySelectorAll('#'+ id + ' option:checked');
+    
+        document.getElementById(options[0].dataset.category).src = "img/" + options[0].dataset.category + "/" + options[0].dataset.img + ".jpg";
+
+        if(options[0].dataset.category == 'side-color') {
+            sideColorPrice = options[0].dataset.price;
+            total = parseFloat(sideColorPrice) + parseFloat(topColorPrice) + parseFloat(ribColorPrice);
+        } else if(options[0].dataset.category == 'top-color') {
+            topColorPrice = options[0].dataset.price;
+            total = parseFloat(sideColorPrice) + parseFloat(topColorPrice) + parseFloat(ribColorPrice);
+        } else if(options[0].dataset.category == 'rib-color') {
+            ribColorPrice = options[0].dataset.price;
+            total = parseFloat(sideColorPrice) + parseFloat(topColorPrice) + parseFloat(ribColorPrice);
+        }
+
+        document.getElementById('custom-seat-price').innerHTML = total;
+    }
 }
